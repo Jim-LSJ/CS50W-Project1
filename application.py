d3aadf4fd71ff8e4aa80504e7b611f9a14ad76d4
Jim-LSJ <jim861026@gmail.com>
2019-09-02 16:44:45 +0800

    so tired

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from util import myhash, get_from_goodreads
 from flask import jsonify
+import time
 
 app = Flask(__name__)
 
@@ -86,7 +87,7 @@ def homepage(page):
     if session.get("user") is None:
         return redirect("/")
 
-    books = db.execute("SELECT isbn, title, author, year, id FROM books LIMIT 9 OFFSET 9 * :page", {"page": str((int(page) - 1))})
+    books = db.execute("SELECT isbn, title, author, year, id FROM books LIMIT 9 OFFSET 9 * :page", {"page": str(int(page) - 1)})
     books_list = []
 
     for row in books:
@@ -96,7 +97,7 @@ def homepage(page):
         books_list.append(row_list)
     books.close()
 
-    return render_template("homepage.html", books_list=books_list, page=int(page), user=session['user'][0])
+    return render_template("homepage.html", books_list=books_list, page=int(page), user=session['user'])
 
 @app.route('/search', methods=["GET", "POST"])
 def search():
@@ -105,10 +106,10 @@ def search():
 
     query = request.form.get("query")
     if query is None or query == "":
-        return render_template("search.html", user=session['user'][0])
+        return render_template("search.html", user=session['user'])
     query = "%" + query + "%"
 
-    books = db.execute("SELECT isbn, title, author, year, id FROM books WHERE isbn LIKE :q OR title LIKE :q OR author LIKE :q OR year LIKE :q",
+    books = db.execute("SELECT isbn, title, author, year, id FROM books WHERE isbn LIKE :q OR title LIKE :q OR author LIKE :q OR year LIKE :q LIMIT 30",
                 {"q": query})
     
     books_list = []
@@ -117,35 +118,69 @@ def search():
         for i in range(5):
             row_list.append(row[i])
         books_list.append(row_list)
+        if len(books_list) == 30:
+            break
     books.close()
 
-    return render_template("search_result.html", books_list=books_list, user=session['user'][0])
+    return render_template("search_result.html", books_list=books_list, user=session['user'])
+
+@app.route('/top10', methods=["GET", "POST"])
+def top10():
+    if session.get("user") is None:
+        return redirect("/")
+
+    books = db.execute("SELECT books.isbn, books.title, books.author, books.year, books.id, COUNT(book_id) FROM books JOIN reviews ON book_id = books.id \
+                        GROUP BY books.isbn, books.title, books.author, books.year, books.id ORDER BY COUNT(book_id) DESC LIMIT 10")
+    
+    books_list = []
+    for row in books:
+        row_list = []
+        for i in range(6):
+            row_list.append(row[i])
+        books_list.append(row_list)
+        if len(books_list) == 30:
+            break
+    books.close()
+
+    return render_template("top10.html", books_list=books_list, user=session['user'])
 
 @app.route("/book/<error>/<id>", methods=["GET", "POST"])
 def book(id, error = None):
     if session.get("user") is None:
         return redirect("/")
+    
+    try:
+        if int(id) > 5000 or int(id) <= 0:
+            return render_template("search_result.html", books_list=[], user=session['user'])
+    except:
+        return render_template("search_result.html", books_list=[], user=session['user'])
 
     if request.method == "GET":
         book_query = db.execute("SELECT isbn, title, author, year, id FROM books WHERE id = :id",
             {"id": id})
         book_info = dict(book_query.first())
 
-        avg_rating, work_ratings_count = get_from_goodreads(book_info['isbn'])
-        book_info['avg_rating'] = avg_rating
-        book_info['work_ratings_count'] = work_ratings_count
+        average_rating, reviews_count = get_from_goodreads(book_info['isbn'])
+        book_info['average_rating'] = average_rating
+        book_info['reviews_count'] = reviews_count
 
-        review_query = db.execute("SELECT reviewer, rating, review_text FROM reviews where book_id = :id",
+        review_query = db.execute("SELECT reviewer, rating, review_text, review_time FROM reviews where book_id = :id",
             {"user": session["user"], "id": id})
         
         reviews = []
         for review in review_query:
             reviews.append(dict(review))
 
-        if error != "0":
+        if error == "0":
+            duplication = "1"
+            error = None
+        elif error == "2":
+            duplication = None
+        else:
+            duplication = None
             error = None
 
-        return render_template("book.html", book_info=book_info, reviews=reviews, error=error, user=session['user'][0])
+        return render_template("book.html", book_info=book_info, reviews=reviews, error=error, duplication=duplication, user=session['user'])
     else:
         review_query = db.execute("SELECT reviewer, rating, review_text FROM reviews where reviewer = :user AND book_id = :id",
             {"user": session["user"], "id": id})
@@ -153,9 +188,12 @@ def book(id, error = None):
         if review_query.first() is None:
             rating = request.form.get("rating")
             review_text = request.form.get("review_text")
+            if not review_text:
+                return redirect("/book/2/" + id)
 
-            db.execute("INSERT INTO reviews (book_id, reviewer, rating, review_text) VALUES (:id, :user, :rating, :review_text)",
-                {"id": id, "user": session["user"], "rating": rating, "review_text": review_text})
+            review_time = time.asctime( time.localtime(time.time()) )
+            db.execute("INSERT INTO reviews (book_id, reviewer, rating, review_text, review_time) VALUES (:id, :user, :rating, :review_text, :review_time)",
+                {"id": id, "user": session["user"], "rating": rating, "review_text": review_text, "review_time": review_time})
             db.commit()
             return redirect("/book/1/" + id)
         else:
--- util.py
+++ util.py
@@ -11,10 +11,10 @@ def get_from_goodreads(isbn):
     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "KQQkXflL5QHzU0ReKHopwQ", "isbns": isbn})
     books = res.json()['books']
     average_rating = books[0]['average_rating']
-    review_count = books[0]['reviews_count']
+    reviews_count = books[0]['reviews_count']
     if average_rating is None:
         average_rating = "Not found"
-    if review_count is None:
-        review_count = "Not found"
+    if reviews_count is None:
+        reviews_count = "Not found"
 
-    return average_rating, review_count
+    return average_rating, reviews_count
